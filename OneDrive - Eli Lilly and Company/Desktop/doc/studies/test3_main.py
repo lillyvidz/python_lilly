cfb729ee6586f88535c594487e78b7131f18fd36
lillyvidz <113332562+lillyvidz@users.noreply.github.com>
2022-09-15 14:33:36 +0530

    Importing function(salary)
    file name:test3_main.py and test3si.py

---
+++ OneDrive - Eli Lilly and Company/Desktop/doc/studies/test3_main.py
@@ -0,0 +1,8 @@
+import test3si
+
+def salaryy():
+    print("hra:",test3si.hra())
+    print("DA:",test3si.da()) 
+    print("compen",test3si.compensation())
+    #print(test3si.da + test3si.hra)  
+salaryy()
---
+++ OneDrive - Eli Lilly and Company/Desktop/doc/studies/test3si.py
@@ -0,0 +1,26 @@
+
+
+"""def interest():
+    principal = float(input('Enter the principle amount: '))
+    time = float(input('Enter the time: '))
+    rate = float(input('Enter the rate: '))
+    simple_interest = (principal*time*rate)/100
+    print("Simple interest is:", simple_interest)
+    return interest
+interest()"""
+sal=int(input("Enter salary:"))
+    
+def hra():
+    hra=(sal*40) / 100
+    return hra
+
+
+def da():
+    da=(sal*10) / 100
+    return da
+
+def compensation():
+    c=(sal*20)/100
+    return(sal+c)
+
+
